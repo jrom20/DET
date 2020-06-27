b5012dd031d0f9b6f2fd38ee3d7ffbb6720c866d
Juan José Romero <jromero@krescor.com>
2020-06-26 22:23:10 -0600

    Queue priority

---
+++ Clase1.ED/Arista.h
@@ -0,0 +1,17 @@
+#pragma once
+
+using namespace std;
+#include<string>
+
+class Arista
+{
+public:
+	string Id;
+	int Distancia;
+
+	Arista(string id, int distancia): Id(id), Distancia(distancia){ }
+
+};
+
+
+
--- Clase1.ED/Clase1.ED.cpp
+++ Clase1.ED/Clase1.ED.cpp
@@ -11,6 +11,8 @@
 #include "HashTableVectorNodo.h"
 #include "MyHashTable.h"
 #include "ArbolBinario.h"
+#include "Arista.h"
+#include <queue>
 
 using std::string;
 using std::cout;
@@ -23,177 +25,28 @@ T max(T const& t1, T const& t2) {
 
 int main()
 {
-	//int valor1 = 1;
-	//int valor2 = 2;
-	//cout << "mayor de 2 y 1 es " << max(valor1, valor2) << endl;
-	//double dValor1 = 1.0;
-	//double dValor2 = 2.0;
-	//cout << "mayor de 2 y 1 es " << max(dValor1, dValor2) << endl;
-
-	//Person persona1("Selvin", "Rodriguez", 50);
-	//Person persona2("Gabriela", "Diaz", 60);
-
-	//cout << "El mayor entre " << persona1.GetNombre()
-	//	<< " y " << persona2.GetNombre() << " es " 
-	//	<< max(persona1, persona2).GetNombre() << endl;
-
-	//
-	//Acumulador<int> numeros(10);
-	//cout << "valor inicial: " << numeros.GetTotal() << endl;
-	//numeros += 9;
-	//numeros += 9;
-	//cout << "valor despues de sumar 18: " << numeros.GetTotal() << endl;
-
-	//Acumulador<string> cadena("");
-	//cadena += "Hola ";
-	//cadena += "Mundo";
-
-	//cout << "Valor de variable cadena: " << cadena.GetTotal() << endl;
-
-	//Acumulador<Person> personas(0);
-	//personas += persona1;
-
-	//cout << "Valor despues de sumar persona1: " << personas.GetTotal() << endl;
-	////personas += persona2;
-
-	//personas += persona2;
-
-	//cout << "Valor despues de sumar persona2: " << personas.GetTotal() << endl;
-	////personas += persona2;
-
-	//MiLista<string> listaAlumnos;
-	//listaAlumnos.Add("Selvin");
-	//listaAlumnos.Add("Gabriela");
-	//listaAlumnos.Add("Delia");
-	//listaAlumnos.Add("Bilander");
-
-	//Nodo<string>* cursor = listaAlumnos.FirstOrDefault();
-
-	//while (cursor != nullptr) {
-	//	cout << "Nombre de alumno: " << cursor->GetValor() << endl;
-	//	cursor = cursor->GetSiguiente();
-	//}
-
-	//listaAlumnos.RemoveLast();
-	//cout << "-------------------" << endl;
-
-	//cursor = listaAlumnos.FirstOrDefault();
-
-	//while (cursor != nullptr) {
-	//	cout << "Nombre de alumno: " << cursor->GetValor() << endl;
-	//	cursor = cursor->GetSiguiente();
-	//}
-
-	//cout << "-------------------" << endl;
-
-	///*
-	//listaAlumnos.RemoveFirst();
-	//listaAlumnos.RemoveFirst();
-
-	//cursor = listaAlumnos.FirstOrDefault();
-
-	//while (cursor != nullptr) {
-	//	cout << "Nombre de alumno: " << cursor->GetValor() << endl;
-	//	cursor = cursor->GetSiguiente();
-	//}
-
-	//cout << "-------------------" << endl;*/
-
-	//listaAlumnos.Remove("Delia");
-
-	//cursor = listaAlumnos.FirstOrDefault();
-
-	//while (cursor != nullptr) {
-	//	cout << "Nombre de alumno: " << cursor->GetValor() << endl;
-	//	cursor = cursor->GetSiguiente();
-	//}
-
-	//MiListaDoble<string> listaDobleAlumnos;
-	//listaDobleAlumnos.Add("Selvin");
-	//listaDobleAlumnos.Add("Gabriela");
-	//listaDobleAlumnos.Add("Delia");
-	//listaDobleAlumnos.Add("Bilander");
-
-	//NodoDoble<string>* _cursor = listaDobleAlumnos.FirstOrDefault();
-
-	//while (_cursor != nullptr) {
-	//	cout << "Nombre de alumno: " << _cursor->GetValor() << endl;
-	//	_cursor = _cursor->GetSiguiente();
-	//}
-
-	//MyStack<string> miPila;
-	//miPila.Push("Selvin");
-	//miPila.Push("Gabriela");
-	//miPila.Push("Delia");
-	//miPila.Push("Bilander");
-
-	//while (miPila.Count != 0) {
-	//	string nombreAlumno = miPila.Pop();
-	//	cout << "Nombre de alumno: " << nombreAlumno << endl;
-	//}
-
-	/*MyQueue<string> miCola;
-	miCola.Enqueue("Selvin");
-	miCola.Enqueue("Gabriela");
-	miCola.Enqueue("Delia");
-	miCola.Enqueue("Bilander");
-
-	while (miCola.Count != 0) {
-		string nombreAlumno = miCola.Dequeue();
-		cout << "Nombre de alumno: " << nombreAlumno << endl;
-	}*/
-
-	//HashTableVectorNodo<string, int> nodo;
-	//nodo.Add("0501", 1);
-	//nodo.Add("0502", 2);
-	//nodo.Add("0503", 3);
-	//nodo.Add("0504", 4);
-
-	/*MyHashTable<string, int> table;
-	table.Add("0501", 1);
-	table.Add("0502", 2);
-	table.Add("0503", 3);
-	table.Add("0504", 4);
-
-	int valorAlmacenado;
-	bool obtenido = table.TryGetValue("050003", valorAlmacenado);*/
-
-	////| - - |
-	////////////////| - 21 - |//////////////
-    ///////////////////////////////////
-	//////| - 4 - |////////| - 77 - |////
-	/////////////////////////////////////
-	/////////////////| -37 - |
-	///////////////////////////////////
-	///////////////////////| - 51 - |
-
 	
-	ArbolBinario arbolito;
+	auto compare = [](Arista a, Arista b) { return a.Distancia > b.Distancia; };
+	priority_queue<Arista, std::vector<Arista>, decltype(compare)> openSet(compare);
 
-	arbolito.Agregar("4");
-	arbolito.Agregar("2");
-	arbolito.Agregar("1");
-	arbolito.Agregar("3");
-	arbolito.Agregar("8");
-	arbolito.Agregar("6");
-	arbolito.Agregar("5");
-	arbolito.Agregar("7");
+	Arista item("12", 10);
+	Arista item1("2", 15);
+	Arista item2("3", 20);
+	Arista item3("4", 30);
+	Arista item4("5", 16);
+	
+	openSet.push(item);
+	openSet.push(item1);
+	openSet.push(item2);
+	openSet.push(item3);
+	openSet.push(item4);
 
-	arbolito.Eliminar("8");
-
-
-	ArbolBinario arbolito2;
-
-	arbolito2.Agregar("4");
-	arbolito2.Agregar("2");
-	arbolito2.Agregar("1");
-	arbolito2.Agregar("3");
-	arbolito2.Agregar("6");
-	arbolito2.Agregar("5");
-	arbolito2.Agregar("8");
-	arbolito2.Agregar("7");
-
-	arbolito2.Eliminar("6");
+	while (!openSet.empty())
+	{
+		auto savedItem = openSet.top();
+		cout << "Id: " << savedItem.Id << " - Distancia: " << savedItem.Distancia << endl;
+		openSet.pop();
+	}
 
 	return 0;
 }
